fc04f6651be384805900be68277c6c48339fcd38
Omarv <omarkval@gmail.com>
2016-03-12 19:27:35 +0000

    tab

--- assignment-3/src/components/sellers/SellersController.spec.js
+++ assignment-3/src/components/sellers/SellersController.spec.js
@@ -2,4 +2,18 @@
 
 describe("SellersController should be unit tested here", function() {
 	// TODO: add beforeEach/describe/it/etc. functions as appropriate!
+	var ctrl;
+	var scope;
+	beforeEach(module("project3App"));
+	beforeEach(inject(function($controller, $rootScope) {
+		scope = $rootScope.$new(); 
+		ctrl = $controller("SellersController", {
+			$scope: scope,
+			AppResorce: AppResorce
+		});
+
+	}));
+	it("Add", function() {
+		
+	});
 });
