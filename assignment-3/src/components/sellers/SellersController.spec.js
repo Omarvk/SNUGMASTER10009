b274091668ae9c49d90139d15a4bd0af8d7e7c6b
Omarv <omarkval@gmail.com>
2016-03-12 22:43:44 +0000

    WOAWOS

--- assignment-3/src/components/sellers/SellersController.spec.js
+++ assignment-3/src/components/sellers/SellersController.spec.js
@@ -9,11 +9,30 @@ describe("SellersController should be unit tested here", function() {
 		scope = $rootScope.$new(); 
 		ctrl = $controller("SellersController", {
 			$scope: scope,
-			AppResorce: AppResorce
 		});
 
 	}));
-	it("Add", function() {
-		
+
+	it("ctrl is Defined", function() {
+		expect(ctrl).toBeDefined();
 	});
+	
+	it("Loading is Defined and Edited", function() {
+		expect(scope.isLoading).toBeDefined();
+		expect(scope.edited).toBeDefined();
+	});
+
+	it("loading is false and Edited", function() {
+		expect(scope.isLoading).toBe(false);
+		expect(scope.edited).toBe(false);
+	});
+
+	it("getSellers", inject(function(AppResource){
+		var sellers = AppResource.getSellers();
+		expect(sellers).not.toBeNull();
+	}));
+	/*
+	it("Defined", inject(function(ctrl) {
+		expect(ctrl).toBeDefined();
+	})); */
 });
